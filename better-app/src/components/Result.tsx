06335071dd00b2ec022b6aaa16ee8b223bfe2e64
apoorvarajan <apoorvarajan1997@gmail.com>
2023-02-27 13:48:08 -0500

    fix

--- better-app/src/components/Result.tsx
+++ better-app/src/components/Result.tsx
@@ -132,7 +132,7 @@ class Results extends React.Component<any,any>{
                                     Task Statement:
                                 </td>
                                 <td className="val1">
-                                    {sel_task && sel_task.taskStmt}
+                                    {subRes.taskStmt}
                                 </td>
                             </tr>
                             <tr className="row1">
@@ -140,7 +140,7 @@ class Results extends React.Component<any,any>{
                                     Request Text:
                                 </td>
                                 <td className="val1">
-                                    {sel_req && sel_req.reqText}
+                                    {subRes.reqText}
                                 </td>
                             </tr>
                             <tr className="row1">
